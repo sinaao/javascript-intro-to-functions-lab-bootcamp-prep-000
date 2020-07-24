5fb4c1783a2c31209f0f9415db55bbe19800f232
sina ao <electronicnewss@gmail.com>
2020-07-24 21:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function sayHiToGrandma(string){
   else if(string===string.toUpperCase()){
     return "YES INDEED!" 
   }else(string==="I love you, Grandma."){
-    return 
+    return  "I love you, too." 
   }
 }

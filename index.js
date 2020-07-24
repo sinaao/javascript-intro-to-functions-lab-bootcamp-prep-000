89e56eaf33cf8915546c202dcffcdffba31b92fe
sina ao <electronicnewss@gmail.com>
2020-07-24 21:28:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function sayHiToGrandma(string){
   }
   else if(string===string.toUpperCase()){
     return "YES INDEED!" 
-  }else(string===`I love you, Grandma.`){
+  }else(string===`I love you, Grandma.`)
     return  "I love you, too." 
-  }
 }
